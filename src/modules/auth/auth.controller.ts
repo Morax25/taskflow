effa7c3bca39d5394a782fcaa29a21954b1fd7b3
morax25 <adarsh@infoneotech.com>
2026-08-24 03:01:51 +0530

    added register controller and routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,16 @@
 import asyncHandler from "../../utils/asyncHandler.js";
 import ApiError from '../../utils/ApiError.js'
 import redis from "../../config/redis.js";
+import { registerUser } from "./auth.services.js";
+import ApiResponse from "../../utils/ApiResponse.js";
 
 export const userRegister = asyncHandler(async(req, res)=>{
-    res.end(201)
+    const {name, email, password} = req.body
+    const result = await registerUser({
+        name, email, password
+    })
+    return res.status(201).json(new ApiResponse({message:"User registered successfully", data:result}))
 })
-
 export const userLogin = asyncHandler(async(req, res)=>{
     const result = await redis.ping()
     res.status(200).json({message:result})
--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -1,1 +1,36 @@
-import prisma from '../../config/prisma.js'
+import { DomainError } from '../../utils/domainError.js';
+import prisma from '../../config/prisma.js';
+import { HttpCode } from '../../utils/statusCode.js';
+import { RegisterUserInput } from '../../types/user.types.js';
+
+export const registerUser = async ({
+  name,
+  email,
+  password,
+}: RegisterUserInput) => {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    throw new DomainError('CONFLICT', 'User with this email already exists', );
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash:password,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+    },
+  });
+
+  return user;
+};
